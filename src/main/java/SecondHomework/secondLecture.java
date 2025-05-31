3c52f18086891db7937a95f182e2b2541d6a60b7
Lela Gugeshashvili <legugeshashvili@tbcbank.com.ge>
2025-05-31 20:55:42 +0400

    Third Homework

--- src/main/java/SecondHomework/secondLecture.java
+++ src/main/java/SecondHomework/secondLecture.java
@@ -4,24 +4,26 @@ public class secondLecture {
         System.out.println("ამოცანა 1:");
 int a=5, b=7, c;
         System.out.println("პირველი ვარიანტი ცვლილებამდე a="+a+" b="+b);
-c=a;
-a=b;
-b=c;
+c=a; // 5
+a=b; // 7
+b=c; // 5
 
         System.out.println("პირველი ვარიანტი ცვლილების შემდეგ a="+a+" b="+b);
+
 
 //ამოცანა 1;  მეორე ვარიანტი, დამატებული ცვლადის გარეშე
 
         int x=8, y=9;
         System.out.println("მეორე ვარიანტი ცვლილებამდე x="+x+" y="+y);
-x=x+y;
-y=x-y;
-x=x-y;
+x=x+y; //8+9=17
+y=x-y; //17-9=8
+x=x-y; //17-8=9
         System.out.println("მეორე ვარიანტი ცვლილების შემდეგ x="+x+" y="+y);
 
         System.out.println("ამოცანა 2:");
 //ამოცანა 2; წელი ნაკიანია, თუ ის იყოფა 4-ზე და არ იყოფა 100-ზე, გარდა იმ შემთხვევისა, როცა ის იყოფა 400-ზე.
         int year=2025;
+
         boolean answer=(year%4==0&&year%100!=0)||(year%400==0);
         System.out.println("2025 წელი ნაკიანია? "+answer);
 
@@ -70,6 +72,8 @@ int A1=3, B1=2, C1=1;
         System.out.println("ცვლილების შემდეგ A1="+A1+", B1="+B1+", C1="+C1);
 //
         System.out.println("Logic1=A1<B1  "+(A1<B1)+", Logic2=B1<C1  "+(B1<C1)+", Logic3=A1<C1  "+(A1<C1));
+
+
 
 
     }
